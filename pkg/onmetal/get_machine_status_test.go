a0de93c4821332b57537c176d6aee645f7ce036e
ashughorla <ashughorla@gmail.com>
2023-01-24 18:14:41 +0530

    Adding negative test cases and comments for onmetal

--- pkg/onmetal/get_machine_status_test.go
+++ pkg/onmetal/get_machine_status_test.go
@@ -18,11 +18,18 @@ import (
 	"fmt"
 
 	"github.com/gardener/machine-controller-manager/pkg/util/provider/driver"
+	"github.com/gardener/machine-controller-manager/pkg/util/provider/machinecodes/codes"
+	"github.com/gardener/machine-controller-manager/pkg/util/provider/machinecodes/status"
 	"github.com/onmetal/machine-controller-manager-provider-onmetal/api/v1alpha1"
 	"github.com/onmetal/machine-controller-manager-provider-onmetal/pkg/internal"
 	testutils "github.com/onmetal/onmetal-api/utils/testing"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+const (
+	FailAtNoKubeconfig string = "machine codes error: code = [Internal] message = [no kubeconfig found in machine secret"
 )
 
 var _ = Describe("GetMachineStatus", func() {
@@ -30,6 +37,45 @@ var _ = Describe("GetMachineStatus", func() {
 	ns, providerSecret, drv := SetupTest(ctx)
 
 	It("should create a machine and ensure status", func() {
+		By("check empty request")
+		emptyMacReq := &driver.GetMachineStatusRequest{
+			Machine:      nil,
+			MachineClass: nil,
+			Secret:       nil,
+		}
+		ret, err := (*drv).GetMachineStatus(ctx, emptyMacReq)
+		Expect(ret).To(BeNil())
+		Expect(err).Should(MatchError(status.Error(codes.InvalidArgument, "received empty request")))
+
+		By("check machineClass provider")
+		getMacReq := &driver.GetMachineStatusRequest{
+			Machine:      newMachine(ns, "machine", -1, nil),
+			MachineClass: newMachineClass(internal.ProviderSpec),
+			Secret:       providerSecret,
+		}
+		getMacReq.MachineClass.Provider = "nonOnmetal"
+		ret, err = (*drv).GetMachineStatus(ctx, getMacReq)
+		Expect(ret).To(BeNil())
+		Expect(err).Should(MatchError(status.Error(codes.InvalidArgument, fmt.Sprintf("requested provider '%s' is not suppored by the driver '%s'", getMacReq.MachineClass.Provider, v1alpha1.ProviderName))))
+
+		By("check namespace in secret")
+		getMacReq.MachineClass.Provider = "onmetal"
+		namespace := getMacReq.Secret.Data["namespace"]
+		delete(getMacReq.Secret.Data, "namespace")
+		ret, err = (*drv).GetMachineStatus(ctx, getMacReq)
+		Expect(ret).To(BeNil())
+		Expect(err).Should(MatchError(status.Error(codes.Internal, fmt.Sprintf("failed to find namespace is machine secret %s", client.ObjectKeyFromObject(getMacReq.Secret)))))
+		getMacReq.Secret.Data["namespace"] = namespace
+
+		By("creating k8sclient from secret")
+		kubeconfig := getMacReq.Secret.Data["kubeconfig"]
+		delete(getMacReq.Secret.Data, "kubeconfig")
+		ret, err = (*drv).GetMachineStatus(ctx, getMacReq)
+		fmt.Println(err)
+		Expect(ret).To(BeNil())
+		Expect(err).Should(MatchError(status.Error(codes.Internal, fmt.Sprintf("failed to create k8s client for machine secret %s: %s %s%s", client.ObjectKeyFromObject(getMacReq.Secret), FailAtNoKubeconfig, client.ObjectKeyFromObject(getMacReq.Secret), "]"))))
+		getMacReq.Secret.Data["kubeconfig"] = kubeconfig
+
 		By("creating machine")
 		Expect((*drv).CreateMachine(ctx, &driver.CreateMachineRequest{
 			Machine:      newMachine(ns, "machine", -1, nil),
