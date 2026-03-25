584b972cfa97cca57d995c77fe1325d8f3180066
Lukas Frank <lukas.frank@sap.com>
2026-03-25 09:34:03 +0100

    Update controller to align with changed API

--- pkg/ironcore/create_machine.go
+++ pkg/ironcore/create_machine.go
@@ -159,7 +159,16 @@ func (d *ironcoreDriver) applyIronCoreMachine(ctx context.Context, req *driver.C
 	}
 
 	if providerSpec.RootDisk == nil {
-		ironcoreMachine.Spec.Image = providerSpec.Image
+		ironcoreMachine.Spec.Volumes = []computev1alpha1.Volume{
+			{
+				Name: "root",
+				VolumeSource: computev1alpha1.VolumeSource{
+					LocalDisk: &computev1alpha1.LocalDiskVolumeSource{
+						Image: providerSpec.Image,
+					},
+				},
+			},
+		}
 	} else {
 		ironcoreMachine.Spec.Volumes = []computev1alpha1.Volume{
 			{
@@ -175,6 +184,11 @@ func (d *ironcoreDriver) applyIronCoreMachine(ctx context.Context, req *driver.C
 									corev1alpha1.ResourceStorage: providerSpec.RootDisk.Size,
 								},
 								Image: providerSpec.Image,
+								DataSource: storagev1alpha1.VolumeDataSource{
+									OSImage: &storagev1alpha1.OSDataSource{
+										Image: providerSpec.Image,
+									},
+								},
 							},
 						},
 					},
