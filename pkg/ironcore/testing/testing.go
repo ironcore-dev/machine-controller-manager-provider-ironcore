d2d63acdac6eb0a1f149ba565e6f16d7cceafc61
Andreas Fritzler <andreas.fritzler@sap.com>
2026-04-14 11:55:41 +0200

    Revert ignition fcos version to v1.3.0 (#532)

--- pkg/ironcore/testing/testing.go
+++ pkg/ironcore/testing/testing.go
@@ -38,7 +38,7 @@ var (
 
 	SampleIgnition = map[string]interface{}{
 		"ignition": map[string]interface{}{
-			"version": "3.6.0",
+			"version": "3.2.0",
 		},
 		"passwd": map[string]interface{}{
 			"users": []interface{}{
